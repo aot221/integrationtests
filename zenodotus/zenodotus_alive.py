de1e4fa1c111bdc9374b0e601d5273b39ee7cdd4
Leonard <llaw02@students.poly.edu>
2014-02-12 20:19:25 +0000

    Zenodotus monitoring test  * sebass's script that has been running for a while but not checked in.

---
+++ zenodotus/zenodotus_alive.py
@@ -0,0 +1,40 @@
+import integrationtestlib
+import send_gmail
+import os
+import sys
+
+def main():
+  # Initialize the gmail setup.
+  success, explanation_str = send_gmail.init_gmail()
+
+  if not success:
+    integrationtestlib.log(explanation_str)
+    sys.exit(0)
+
+  print "Beginning query."
+  success = True
+
+  try:
+    # Query zenodotus
+    for line in os.popen('dig @8.8.8.8 zenodotus.poly.edu', 'r').readlines():
+      # Can't do direct comparison between dig's output and hardcoded string
+      # TTL value changes on each call.  Instead, check if any of the lines
+      # contain the entry for blackbox.
+      if 'blackbox.poly.edu.' in line.split() and '128.238.63.50' in line.split():
+        break
+    else:
+      print "Zenodotus failed to respond properly!"
+      # Query is invalid!
+      success = False
+      integrationtestlib.notify("Error: Zenodotus has failed to correctly respond; the machine has likely been rebooted. Please restart the zenodotus server on zenodotus@blackbox. This report will be re-sent hourly while the problem persists.", "Cron: Zenodotus failure")
+  except Exception, e:
+    print "Unknown error!"
+    print str(e)
+    success = False
+    integrationtestlib.notify("Error: Zenodotus seems to be down! Error data: " + str(e), "Cron: Zenodotus failure")
+
+  if success:
+    print "Query was successful."
+
+if __name__ == "__main__":
+  main()
